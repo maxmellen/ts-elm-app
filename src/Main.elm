036878843e5c582b7171f5ec23dc7449eeb2ff12
Maximilien Mellen <maxmellen0@gmail.com>
2018-09-14 16:43:37 +0200

    Write basic Elm app

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,44 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Browser
+import Html exposing (Html, button, div, p, text)
+import Html.Events exposing (onClick)
 
 
-main : Html msg
+type alias Model =
+    Int
+
+
+type Msg
+    = Increment
+
+
+init : Model
+init =
+    0
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ p [] [ text <| "Counter: " ++ String.fromInt model ]
+        , p []
+            [ button [ onClick Increment ] [ text "Increment" ]
+            ]
+        ]
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+
+main : Program () Model Msg
 main =
-    text "Hello Elm!"
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
